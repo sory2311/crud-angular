a4c10a2f0a8d1186a3475472f59f5b0cc28337c1
Sorangel Mejia <mejiasory96@gmail.com>
2021-05-25 12:17:18 -0400

    guardando cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,25 @@ export class AppComponent {
     {id: 3, name: "Bruce", country: "Chile"},
   ];
   selectedEmployee: Employee= new Employee();
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    
+    }
+    this.selectedEmployee = new Employee();
+  }
+  delete(){
+    if(confirm ('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+    }
+  }
+    
+
 }
